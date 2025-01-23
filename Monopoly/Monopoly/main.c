6b98804399ed9df04fb3f51430cfb5c3ea1938c3
SchneiderAaron <aaron.schneider@stud.msw.ch>
2025-01-23 07:11:52 +0100

    Monopoly File erstellt

---
+++ Monopoly/Monopoly/main.c
@@ -0,0 +1,18 @@
+/*
+ * Monopoly.c
+ *
+ * Created: 23.01.2025 07:11:39
+ * Author : e1Schnei
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
